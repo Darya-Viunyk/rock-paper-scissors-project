65605addf8da3948516d6901d519e50f5521b924
Darya-Viunyk <dar.viunyk@gmail.com>
2023-07-26 19:37:17 +0100

    added task

---
+++ rock-paper-scissors-project/src/main/java/com/example/rockpaperscissorsproject/RockPaperScissorsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.rockpaperscissorsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissorsProjectApplication.class, args);
+	}
+
+}
